a4d98f03bf0734d8d96c284e713fad4b0959b211
Yuvraj686 <yuvigarg6806@gmail.com>
2025-01-05 19:21:54 +0530

    Add files via upload

---
+++ project.cpp
@@ -0,0 +1,11 @@
+10 enqueued to queue
+20 enqueued to queue
+30 enqueued to queue
+40 enqueued to queue
+50 enqueued to queue
+Queue elements: 10 20 30 40 50
+Dequeued: 10
+Queue elements: 20 30 40 50
+60 enqueued to queue
+Queue elements: 20 30 40 50 60
+
---
+++ script.js
@@ -0,0 +1,17 @@
+var slideIndex = 0;
+var slides = document.querySelectorAll('#slideshow img');
+
+function showSlides() {
+slideIndex++;
+if (slideIndex >= slides.length) {
+slideIndex = 0;
+}
+
+for (var i = 0; i < slides.length; i++) {
+slides[i].classList.remove('active');
+}
+
+slides[slideIndex].classList.add('active');
+}
+
+setInterval(showSlides, 2000);
